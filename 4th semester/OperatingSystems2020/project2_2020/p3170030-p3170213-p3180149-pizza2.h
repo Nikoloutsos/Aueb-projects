5762b74efafc7bf4ea873ea881a5ec9bb4254c26
Michael Papapetros <40656872+michalispap@users.noreply.github.com>
2020-05-21 16:13:04 +0300

    Add files via upload

---
+++ 4th semester/OperatingSystems2020/project2_2020/p3170030-p3170213-p3180149-pizza2.h
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <pthread.h>
+#include <stdlib.h>
+#include <time.h>
+#include <unistd.h>
+
+#define n_cook 2
+#define n_oven 5
+#define n_deliverer 5
+#define t_orderlow 1
+#define t_orderhigh 5
+#define n_orderlow 1
+#define n_orderhigh 5
+#define t_prep 1
+#define t_bake 10
+#define t_low 5
+#define t_high 15
+#define BILLION 1000000000L
+
---
+++ 4th semester/OperatingSystems2020/project2_2020/test-res2.sh
@@ -0,0 +1,2 @@
+gcc -Wall -pthread p3170030-p3170213-p3180149-pizza2.c
+./a.out 100 1000
